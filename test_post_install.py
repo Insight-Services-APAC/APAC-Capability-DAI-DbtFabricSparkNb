8b1bbf4a449257ff6a43e87a97f07a0a86dab45c
Grant Krieger <Grant.Krieger@insight.com>
2024-05-29 11:33:14 +1000

    Removed assets

--- test_post_install.py
+++ test_post_install.py
@@ -13,11 +13,6 @@ config = dbt.config.project.load_raw_project(os.environ['DBT_PROJECT_DIR'])
 profile_info = profile[config['profile']]
 target_info = profile_info['outputs'][profile_info['target']]
 lakehouse = target_info['lakehouse']
-workspacename = target_info['workspacename']
-
-
-lakehousedatapath = target_info['lakehousedatapath']
-lakehousedatapathfull = lakehouse + ".Lakehouse" + lakehousedatapath
 
 shutil.rmtree(os.environ['DBT_PROJECT_DIR'] + "/target")
 utils.GenerateAzCopyScripts(os.environ['DBT_PROJECT_DIR'], target_info['workspaceid'], target_info['lakehouseid'])
@@ -29,8 +24,4 @@ utils.GenerateMasterNotebook(os.environ['DBT_PROJECT_DIR'], target_info['workspa
 utils.GenerateMetadataExtract(os.environ['DBT_PROJECT_DIR'], target_info['workspaceid'], target_info['lakehouseid'], lakehouse)
 utils.GenerateNotebookUpload(os.environ['DBT_PROJECT_DIR'], target_info['workspaceid'], target_info['lakehouseid'], lakehouse)
 
-# Note: for following to run you must have access to the following fabric workspace and datalake
-# add profile.yml must be same structure as assets\profiles.yml in this repo note new attributes have been added
-# utils.UploadAllNotebooks(workspacename,lakehousedatapathfull)  ##uploads notebooks to onelake
-# spark notebook to conevrt uploaded files into notebooks is assets\PySparkIngestNotebooks.ipynb
-# the above notebook needs assets\assets\fabricnotebookutil.py
+
