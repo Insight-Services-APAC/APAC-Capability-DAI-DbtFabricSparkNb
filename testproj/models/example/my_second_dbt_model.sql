0d4ba19b1fa66b9c33a2681cfde3c661a171364e
Grant Krieger <Grant.Krieger@insight.com>
2024-06-03 15:41:22 +1000

    Added support for Multiple datalakes

--- testproj/models/example/my_second_dbt_model.sql
+++ testproj/models/example/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
-{{ config(materialized='incremental', incremental_strategy="append",file_format="delta") }}
+{{ config(materialized='incremental', incremental_strategy="append",file_format="delta",schema="lakesales2") }}
 select *
 from {{ ref('my_first_dbt_model') }}
 where id = 1
