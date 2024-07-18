5ed0b5aab9c996173879dbd3b9a5c29f46902e6d
Grant Krieger <Grant.Krieger@insight.com>
2024-07-18 13:43:22 +1000

    pip test

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         "azure-core>=1.26.4",
         "requests==2.31.0",
         "msfabricpysdkcore==0.1.1"
-        ## "pyspark>=3.0.0,<4.0.0",
+        ### "pyspark>=3.0.0,<4.0.0",
     ],
     zip_safe=False,
     classifiers=[
