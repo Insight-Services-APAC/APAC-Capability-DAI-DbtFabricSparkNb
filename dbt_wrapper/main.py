6e96cb2365757aeb7839c4ddad4b36ef6a7ba625
John Rampono <john.rampono@insight.com>
2024-07-19 09:20:14 +0000

    Updated dbt_wrapper to include dbt_profiles_dir

--- dbt_wrapper/main.py
+++ dbt_wrapper/main.py
@@ -3,42 +3,68 @@ from typing_extensions import Annotated
 from rich.console import Console
 from rich.theme import Theme
 import dbt.adapters.fabricsparknb.utils as utils
-import os 
+import os
 import sys
 import shutil
 import dbt.adapters
 
 app = typer.Typer(no_args_is_help=True)
 
-custom_theme = Theme({
-    "info": "dim cyan",
-    "warning": "magenta",
-    "danger": "bold red"
-})
+custom_theme = Theme({"info": "dim cyan", "warning": "magenta", "danger": "bold red"})
 
 console = Console(theme=custom_theme)
+
 
 def docs_options():
     return ["generate", "serve"]
 
 
-
 @app.command()
-def build(dbt_project_dir: str, pre_install: Annotated[bool, typer.Option(help="The option to run the dbt adapter using source code and not the installed package.")] = False, upload_notebooks_via_api: Annotated[bool, typer.Option(help="The option to upload your notebooks directly via the powerbi api.")] = False):
-    
+def build(
+    dbt_project_dir: Annotated[
+        str,
+        typer.Argument(
+            help="The path to the dbt_project directory. If left blank it will use the current directory"
+        ),
+    ],
+    dbt_profiles_dir: Annotated[
+        str,
+        typer.Argument(
+            help="The path to the dbt_profile directory. If left blank it will use the users home directory followed by .dbt."
+        ),
+    ] = None,
+    pre_install: Annotated[
+        bool,
+        typer.Option(
+            help="The option to run the dbt adapter using source code and not the installed package."
+        ),
+    ] = False,
+    upload_notebooks_via_api: Annotated[
+        bool,
+        typer.Option(
+            help="The option to upload your notebooks directly via the powerbi api."
+        ),
+    ] = False,
+):
+
     if len(dbt_project_dir.replace("\\", "/").split("/")) > 1:
-        console.print("Warning: :file_folder: The dbt_project_dir provided is nested and not a valid dbt project directory in windows. Copying the dbt_project_dir to the samples_tests directory.", style="warning")
+        console.print(
+            "Warning: :file_folder: The dbt_project_dir provided is nested and not a valid dbt project directory in windows. Copying the dbt_project_dir to the samples_tests directory.",
+            style="warning",
+        )
         old_dbt_project_dir = dbt_project_dir.replace("\\", "/")
         dbt_project_dir = "samples_tests"
         if os.path.exists(dbt_project_dir):
-            shutil.rmtree(dbt_project_dir)            
+            shutil.rmtree(dbt_project_dir)
         shutil.copytree(old_dbt_project_dir, dbt_project_dir)
-    os.environ['DBT_PROJECT_DIR'] = dbt_project_dir
+    os.environ["DBT_PROJECT_DIR"] = dbt_project_dir
+    if (dbt_profiles_dir is not None):
+        os.environ["DBT_PROFILES_DIR"] = dbt_profiles_dir
     utils.RunDbtProject(PreInstall=pre_install, Upload=upload_notebooks_via_api)
 
 
 @app.command()
-def docs(): 
+def docs():
     print(f"Goodbye")
 
 
