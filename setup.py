55f25c40e868700c4406404cf8c7a2b25cf4bb0e
Grant Krieger <Grant.Krieger@insight.com>
2024-07-30 08:16:03 +1000

    #115

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         "azure-core>=1.26.4",
         "requests==2.31.0",
         "typer>=0.12.3",
-        "setuptools>=71.0.4",
+        "setuptools>=72.1.0",
         "azure-storage-file-datalake"
        
    ],
