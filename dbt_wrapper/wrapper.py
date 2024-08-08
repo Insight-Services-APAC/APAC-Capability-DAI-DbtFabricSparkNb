776e6d9c9588c73ba9897b5b37d1e813ae1e34f0
John Rampono <john.rampono@insight.com>
2024-08-08 12:59:20 +0800

    Added in console master execution summary

--- dbt_wrapper/wrapper.py
+++ dbt_wrapper/wrapper.py
@@ -56,7 +56,11 @@ class Commands:
         self.profile_info = self.profile[self.config['profile']]
         self.target_info = self.profile_info['outputs'][self.profile_info['target']]
         self.lakehouse = self.target_info['lakehouse']
-        self.sql_endpoint = self.target_info['sql_endpoint']
+        if "sql_endpoint" in self.target_info.keys():
+            self.sql_endpoint = self.target_info['sql_endpoint']
+        else: 
+            self.sql_endpoint = None
+
         self.project_name = self.config['name']
         #self.workspaceid = self.config['workspaceid']
        
@@ -164,18 +168,21 @@ class Commands:
         self.fa.APIRunNotebook(progress=progress, task_id=task_id, workspace_id=self.target_info['workspaceid'], notebook_name=nb_name)
 
     def GetExecutionResults(self, progress: ProgressConsoleWrapper, task_id):
-        sql = f"""
-            Select a.notebook, replace(CONVERT(varchar(20), DATEADD(second, a.start_time, '1970/01/01 00:00:00'),126), 'T',' ') start_time, status, error
-            from {self.lakehouse}.dbo.execution_log a 
-            join 
-            (
-            Select top 1 batch_id, max(DATEADD(second, start_time, '1970/01/01 00:00:00')) start_time  
-            from {self.lakehouse}.dbo.execution_log  
-            group by batch_id 
-            order by start_time desc
-            ) b on a.batch_id = b.batch_id
-            where a.status = 'error'
-        """
-        _fas = fas.FabricApiSql(console=self.console, server=self.sql_endpoint, database=self.lakehouse)
-        _fas.ExecuteSQL(sql=sql, progress=progress, task_id=task_id)
+        if self.sql_endpoint is not None:
+            sql = f"""
+                Select a.notebook, replace(CONVERT(varchar(20), DATEADD(second, a.start_time, '1970/01/01 00:00:00'),126), 'T',' ') start_time, status, error
+                from {self.lakehouse}.dbo.execution_log a 
+                join 
+                (
+                Select top 1 batch_id, max(DATEADD(second, start_time, '1970/01/01 00:00:00')) start_time  
+                from {self.lakehouse}.dbo.execution_log  
+                group by batch_id 
+                order by start_time desc
+                ) b on a.batch_id = b.batch_id
+                where a.status = 'error'
+            """
+            _fas = fas.FabricApiSql(console=self.console, server=self.sql_endpoint, database=self.lakehouse)
+            _fas.ExecuteSQL(sql=sql, progress=progress, task_id=task_id)
+        else: 
+            progress.print("SQL Endpoint not found in profile. Skipping Execution Results", level=LogLevel.WARNING)
 
