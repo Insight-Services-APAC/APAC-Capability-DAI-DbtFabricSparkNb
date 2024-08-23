ae0ddfceeeef0c8248a85cc3b4ccb4a1066b4965
Kevin Rodriguez <Kevin.Rodriguez@insight.com>
2024-08-23 15:04:38 +1000

    Retrofit Dev and Main changes

--- dbt_wrapper/wrapper.py
+++ dbt_wrapper/wrapper.py
@@ -87,15 +87,6 @@ class Commands:
 
       
     def PrintFirstTimeRunningMessage(self):
-        print('\033[1;33;48m', "It seems like this is the first time you are running this project. Please update the metadata extract json files in the metaextracts directory by performing the following steps:")
-        print(f"1. Run ./{os.environ['DBT_PROJECT_DIR']}/target/pwsh/upload.ps1")
-        print("2. Login to the Fabric Portal and navigate to the workspace and lakehouse you are using")
-        print(f"3. Manually upload the following notebook to your workspace: {os.environ['DBT_PROJECT_DIR']}/target/notebooks/import_{os.environ['DBT_PROJECT_DIR']}_notebook.ipynb. See https://learn.microsoft.com/en-us/fabric/data-engineering/how-to-use-notebook#import-existing-notebooks")
-        print("4. Open the notebook in the workspace and run all cells. This will upload the generated notebooks to your workspace.")
-        print(f"5. A new notebook should appear in the workspace called metadata_{os.environ['DBT_PROJECT_DIR']}_extract.ipynb. Open this notebook and run all cells. This will generate the metadata extract json files in the metaextracts directory.")
-        print(f"6. Run ./{os.environ['DBT_PROJECT_DIR']}/target/pwsh/download.ps1. This will download the metadata extract json files to the metaextracts directory.")
-        print("7. Re-run this script to generate the model and master notebooks.")
-
         print('\033[1;33;48m', "Error!")
         print(f"Directory ./{os.environ['DBT_PROJECT_DIR']}/metaextracts/ does not exist and should have been created automatically.")
 
